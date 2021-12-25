211838d498c59e81f2755d9991f2304efdd9b0da
ilgolf <ilgolc@naver.com>
2021-12-25 22:22:28 +0900

    "회원가입 리펙토링 작업"

---
+++ src/main/java/com/login/project/loginProject/domain/member/domain/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.login.project.loginProject.domain.member.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+}
