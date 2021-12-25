34bbf935c98ead0d3dd99913eb6c12bcbaf67033
ilgolf <ilgolc@naver.com>
2021-12-25 22:28:59 +0900

    "회원가입 요청시 DTO로 정보 받아오기"

--- src/main/java/com/login/project/loginProject/domain/auth/api/MemberApiController.java
+++ src/main/java/com/login/project/loginProject/domain/auth/api/MemberApiController.java
@@ -1,9 +1,27 @@
 package com.login.project.loginProject.domain.auth.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.login.project.loginProject.domain.auth.MemberService;
+import com.login.project.loginProject.domain.member.domain.Member;
+import com.login.project.loginProject.domain.member.dto.MemberDTO;
+import javassist.bytecode.DuplicateMemberException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
+@RequestMapping("/members")
+@RequiredArgsConstructor
 public class MemberApiController {
+
+    private final MemberService memberService;
+
+    // 회원 가입 api
+    @PostMapping("/join")
+    public ResponseEntity<Member> register(@Valid @RequestBody MemberDTO memberDTO) throws DuplicateMemberException {
+        return ResponseEntity.ok(memberService.signUp(memberDTO));
+    }
 
 
 }
