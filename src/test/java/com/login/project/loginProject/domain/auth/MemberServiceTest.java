0c0c0f4052d5f8c6eefe026042e2690676bc878e
ilgolf <ilgolc@naver.com>
2021-12-30 15:17:51 +0900

    "회원 조회 저장 수정 단위테스트 성공"

--- src/test/java/com/login/project/loginProject/domain/auth/MemberServiceTest.java
+++ src/test/java/com/login/project/loginProject/domain/auth/MemberServiceTest.java
@@ -2,29 +2,81 @@ package com.login.project.loginProject.domain.auth;
 
 import com.login.project.loginProject.domain.member.domain.Member;
 import com.login.project.loginProject.domain.member.domain.MemberRepository;
+import com.login.project.loginProject.domain.member.domain.RoleType;
+import com.login.project.loginProject.domain.member.dto.MemberUpdateDTO;
+import javassist.bytecode.DuplicateMemberException;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@Transactional
 class MemberServiceTest {
 
-    @Autowired private MemberRepository memberRepository;
-    @Autowired private MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired MemberService memberService;
+
+    private static Member givenMember;
 
     @AfterEach
-    void tearDown() {
+    void clear() {
         memberRepository.deleteAll();
     }
 
+    Member givenInfo() {
+        return memberRepository.save(givenMember);
+    }
+
     @Test
-    @DisplayName("회원가입 API")
-    void join() {
-        Member.builder()
-                .email("ilgolc@na")
+    void readMember() {
+        // given
+        givenMember = Member.builder()
+                .email("ilgolc@naver.com")
+                .password("1234")
+                .nickName("Golf")
+                .roleType(RoleType.USER)
+                .age(26)
+                .build();
+
+        Member member = givenInfo();
+
+        // when
+        Member newMember = memberService.lookup(member.getId());
+
+        // then
+        assertThat(newMember.getEmail()).isEqualTo("ilgolc@naver.com");
+        assertThat(newMember.getAge()).isEqualTo(26);
+    }
+
+    @Test
+    void updateMember() throws DuplicateMemberException {
+        // given
+        givenMember = Member.builder()
+                .email("ilgolc@naver.com")
+                .password("1234")
+                .nickName("Golf")
+                .roleType(RoleType.USER)
+                .age(26)
+                .build();
+        Member savedMember = givenInfo();
+
+        MemberUpdateDTO updateDTO = new MemberUpdateDTO();
+
+        updateDTO.setEmail("ssar@naver.com");
+        updateDTO.setPassword("12345");
+        updateDTO.setNickName("Dev.Golf");
+
+        // when
+        Member member = memberService.updateMember(savedMember.getId(), updateDTO);
+
+        // then
+        assertThat(member.getEmail()).isEqualTo("ssar@naver.com");
+        assertThat(member.getPassword()).isEqualTo("12345");
+        assertThat(member.getNickName()).isEqualTo("Dev.Golf");
     }
 }
