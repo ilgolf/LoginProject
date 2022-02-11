c6fdec128500ae14828764985b9e6573b8b9b49c
노경태 <ilgolc@naver.com>
2022-02-11 18:23:27 +0900

    "principal Detail fix 빈을 주입하는 방식이 아나므로 일반 생성자로 변경"

--- src/main/java/com/login/project/loginProject/domain/auth/principal/PrincipalDetail.java
+++ src/main/java/com/login/project/loginProject/domain/auth/principal/PrincipalDetail.java
@@ -9,10 +9,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.ArrayList;
 import java.util.Collection;
 
-@RequiredArgsConstructor
 public class PrincipalDetail implements UserDetails {
 
     private final Member member;
+
+    public PrincipalDetail(final Member member) {
+        this.member = member;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
