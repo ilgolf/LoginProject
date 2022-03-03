389d50d30e59abf8b784e1e8ab70d5240f5d9751
Developer_GOLF <77387861+ilgolf@users.noreply.github.com>
2022-03-03 23:08:50 +0900

    "직렬화 시 기본생성자는 필수이다."

--- src/main/java/com/login/project/loginProject/domain/member/dto/MemberUpdateDTO.java
+++ src/main/java/com/login/project/loginProject/domain/member/dto/MemberUpdateDTO.java
@@ -1,10 +1,7 @@
 package com.login.project.loginProject.domain.member.dto;
 
 import com.login.project.loginProject.domain.member.domain.Member;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -13,6 +10,7 @@ import javax.validation.constraints.Size;
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberUpdateDTO {
 
     @NotBlank(message = "이메일을 입력해주세요")
@@ -21,12 +19,12 @@ public class MemberUpdateDTO {
     private String email;
 
     @NotBlank(message = "비밀번호를 입력해주세요")
-    @Size(min = 10, max = 30)
+    @Size(min = 8, max = 30)
     private String password;
 
     @NotBlank(message = "별명을 입력해주세요")
     @Size(min = 4, max = 20)
-    private String nickName;
+    private String nickname;
 
 
 
@@ -34,7 +32,7 @@ public class MemberUpdateDTO {
         return Member.builder()
                 .email(email)
                 .password(password)
-                .nickname(nickName)
+                .nickname(nickname)
                 .build();
     }
 }
