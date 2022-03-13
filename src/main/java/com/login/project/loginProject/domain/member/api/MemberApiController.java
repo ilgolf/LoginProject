c44a311a977ee832b79d414c64ea8b34364bdf0f
Developer_GOLF <77387861+ilgolf@users.noreply.github.com>
2022-03-13 23:29:50 +0900

    "Security Context에서 직접 회원 정보를 가져온다."

--- src/main/java/com/login/project/loginProject/domain/member/api/MemberApiController.java
+++ src/main/java/com/login/project/loginProject/domain/member/api/MemberApiController.java
@@ -1,5 +1,6 @@
 package com.login.project.loginProject.domain.member.api;
 
+import com.login.project.loginProject.domain.auth.principal.CustomUserDetails;
 import com.login.project.loginProject.domain.member.application.MemberService;
 import com.login.project.loginProject.domain.member.domain.Member;
 import com.login.project.loginProject.domain.member.domain.RoleType;
@@ -9,14 +10,10 @@ import com.login.project.loginProject.domain.member.dto.MemberUpdateDTO;
 import javassist.bytecode.DuplicateMemberException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.User;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -33,12 +30,12 @@ import java.util.List;
 public class MemberApiController {
 
     private final MemberService memberService;
-    private final AuthenticationManagerBuilder managerBuilder;
 
     // 회원 정보 api
     @GetMapping("/findByEmail")
-    public ResponseEntity<MemberResponse> findMember(@AuthenticationPrincipal String email) {
-        return ResponseEntity.ok(memberService.findByEmail(email));
+    public ResponseEntity<MemberResponse> findMember() {
+        String email = getEmail();
+        return ResponseEntity.ok(memberService.findByEmail(getEmail()));
     }
 
     // 회원 가입 api
@@ -52,17 +49,16 @@ public class MemberApiController {
 
     // 회원 변경
     @PatchMapping
-    public ResponseEntity<Void> update(@AuthenticationPrincipal String email, @Valid @RequestBody MemberUpdateDTO updateDTO) throws DuplicateMemberException {
+    public ResponseEntity<Void> update(@Valid @RequestBody MemberUpdateDTO updateDTO) throws DuplicateMemberException {
         log.debug("{} : 회원 수정", updateDTO.getEmail());
-        memberService.updateMember(updateDTO.toEntity(), email);
-
+        memberService.updateMember(updateDTO.toEntity(), getEmail());
         return ResponseEntity.ok().build();
     }
 
     // 회원 삭제
     @DeleteMapping
-    public ResponseEntity<Void> delete(@AuthenticationPrincipal String email) {
-        memberService.delete(email);
+    public ResponseEntity<Void> delete() {
+        memberService.delete(getEmail());
         return ResponseEntity.noContent().build();
     }
 
@@ -74,7 +70,9 @@ public class MemberApiController {
         return ResponseEntity.ok(memberService.findAll(pageable));
     }
 
-
+    private String getEmail() {
+        return SecurityContextHolder.getContext().getAuthentication().getName();
+    }
 
     /**
      * 페이징 테스트용 로직
