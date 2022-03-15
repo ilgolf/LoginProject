7ba1c2ace884f49fd3e9878e354c022fcf33bbc3
ilgolf <ilgolc@naver.com>
2022-03-15 16:21:38 +0900

    "import 변경"

--- src/main/java/com/login/project/loginProject/domain/auth/principal/CustomDetailService.java
+++ src/main/java/com/login/project/loginProject/domain/auth/principal/CustomDetailService.java
@@ -1,19 +1,13 @@
 package com.login.project.loginProject.domain.auth.principal;
 
-import com.login.project.loginProject.domain.auth.exception.EmailNotFoundException;
-import com.login.project.loginProject.domain.member.domain.Member;
+import com.login.project.loginProject.domain.member.exception.EmailNotFoundException;
 import com.login.project.loginProject.domain.member.domain.MemberRepository;
 import com.login.project.loginProject.global.error.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
-
-import java.util.Collections;
 
 @Service
 @Slf4j
