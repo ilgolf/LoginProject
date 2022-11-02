e5a5a9a7937a8acd1f80b0bfe29c489d04354c4f
nogyeongtae <nokt@crowdworks.kr>
2022-11-02 21:46:44 +0900

    commit

--- src/main/java/com/login/project/loginProject/domain/member/dto/JoinRequest.java
+++ src/main/java/com/login/project/loginProject/domain/member/dto/JoinRequest.java
@@ -36,9 +36,7 @@ public class JoinRequest {
     @Size(min = 4, max = 20)
     private String nickname;
 
-    @JsonFormat(
-            shape = JsonFormat.Shape.STRING,
-            pattern = "dd-MM-yyyy")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate birth;
 
     public Member toEntity() {
